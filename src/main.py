30ec68b48b44049fd03eb26614048c149cbdf3aa
kcjennin <kcjennin@calpoly.edu>
2022-01-25 13:30:52 -0800

    fixing main

--- src/main.py
+++ src/main.py
@@ -15,9 +15,9 @@ from servo import Servo
 
 def main():
     m_config = MotorConfig('PA10', 'PB4', 'PB5', pyb.Timer(3))
-    e_config = EncoderConfig('PB6', 'PB7', pyb.Timer(8))
+    e_config = EncoderConfig('PC6', 'PC7', pyb.Timer(8))
 
-    servo = Servo(m_config, e_config)
+    servo = Servo(m_config, e_config, 0.00625)
 
     servo.move_to_point(0)
     servo.move_to_point(16000)
