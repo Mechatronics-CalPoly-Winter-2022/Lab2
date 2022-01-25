3f9683b6532f58a1f2bb5cca00ca5680140090e8
Zarek Lazowski <96549302+ZarekLazowski@users.noreply.github.com>
2022-01-25 15:02:45 -0800

    actual code working

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@
 # @date January 11th, 2022
 
 import pyb
+import time
 from enc_driver import EncoderConfig
 from motor_driver import MotorConfig
 from servo import Servo
@@ -15,13 +16,20 @@ from servo import Servo
 
 def main():
     m_config = MotorConfig('PA10', 'PB4', 'PB5', pyb.Timer(3))
-    e_config = EncoderConfig('PC6', 'PC7', pyb.Timer(8))
+    e_config = EncoderConfig('PC6', 'PC7', pyb.Timer(8, prescaler=1, period=100000))
 
     servo = Servo(m_config, e_config, 0.00625)
+    
+    print(servo._e_pin1)
+    print(servo._e_pin2)
+    print(servo._e_ch1)
+    print(servo._e_ch2)
+    print(servo._e_tim)
 
-    servo.enable_motor()
-    servo.set_duty_cycle(50)
-
+    while True:
+        print(servo.get_count())
+        time.sleep(0.5)
+    
 
 if __name__ == '__main__':
     print('Starting main...')
