b2e6f4a4e4aef1b32192b6e823170e725124c85a
kcjennin <kcjennin@calpoly.edu>
2022-01-28 11:00:45 -0800

    Changed to be more like the assignment

--- src/main.py
+++ src/main.py
@@ -8,21 +8,51 @@
 # @date January 11th, 2022
 
 import pyb
+import utime
 from enc_driver import EncoderConfig
 from motor_driver import MotorConfig
 from servo import Servo
+from pid_controller import PID
 
 
 def main():
+    # initialize the encoder and motor drivers
     m_config = MotorConfig('PA10', 'PB4', 'PB5', pyb.Timer(3))
     e_config = EncoderConfig('PC6', 'PC7', pyb.Timer(8))
 
+    # initialize the servo
     servo = Servo(m_config, e_config)
     servo.enable_motor()
     servo.zero()
 
-    servo.move_to_point(31000)
-    servo.move_to_point(17000)
+    # initialize the pid controller
+    # 11488 is 1/3 of the encoder
+    setpoint = 11488
+    pid = PID(setpoint, input('Enter Kp: '))
+
+    # initialize encoder and time data lists
+    encoder_data = []
+    time_data = []
+
+    # five seconds for each third of the encoder
+    for _ in range(3):
+        # set the timeout to 5 seconds
+        timeout = utime.ticks_add(utime.ticks_ms(), 5000)
+        while utime.ticks_diff(timeout, utime.ticks_ms()) > 0:
+            # get the error and adjust the duty cycle
+            servo.set_duty_cycle(pid.update(servo.get_error(11488)))
+
+            # get the encoder data and time data
+            encoder_data.append(servo.read())
+            time_data.append(utime.ticks_ms())
+
+            utime.sleep_ms(10)
+
+        # move the encoder to the next third
+        pid.set_setpoint(pid.setpoint + setpoint)
+
+    # print the encoder data and time data
+    pid.print_data(encoder_data, time_data)
 
     servo.disable_motor()
     
--- src/servo.py
+++ src/servo.py
@@ -14,7 +14,7 @@ class Servo(MotorDriver, EncoderDriver):
 
     gain: float
 
-    def __init__(self, m_config: MotorConfig, e_config: EncoderConfig, gain: float = 0.0058) -> None:
+    def __init__(self, m_config: MotorConfig, e_config: EncoderConfig) -> None:
         '''!
         Creates a servo object by initializing the motor and encoder drivers.
         @param m_config The motor configuration
@@ -24,28 +24,27 @@ class Servo(MotorDriver, EncoderDriver):
 
         MotorDriver.__init__(self, *m_config.args)
         EncoderDriver.__init__(self, *e_config.args)
-        self.gain = gain
 
         print('finished.')
 
-    def move_to_point(self, setpoint: int) -> None:
-        '''!
-        This method moves the servo to the given setpoint.
-        There is a timeout of 5 seconds
-        @param setpoint The desired setpoint
-        '''
-        print('Moving to point...', end=' ')
+    # def move_to_point(self, setpoint: int) -> None:
+    #     '''!
+    #     This method moves the servo to the given setpoint.
+    #     There is a timeout of 5 seconds
+    #     @param setpoint The desired setpoint
+    #     '''
+    #     print('Moving to point...', end=' ')
 
-        # enable motor
-        self.enable_motor()
+    #     # enable motor
+    #     self.enable_motor()
 
-        timeout = time.time() + 5
+    #     timeout = time.time() + 5
 
-        while time.time() < timeout:
-            # set duty cycle according to distance to setpoint and servo gain
-            err = self.get_error(setpoint)
-            pwm = -1 * err * self.gain
-            self.set_duty_cycle(pwm)
-            print(err, self.read())
+    #     while time.time() < timeout:
+    #         # set duty cycle according to distance to setpoint and servo gain
+    #         err = self.get_error(setpoint)
+    #         pwm = -1 * err * self.gain
+    #         self.set_duty_cycle(pwm)
+    #         print(err, self.read())
 
-        print(f'finished at {self.read()}')
+    #     print(f'finished at {self.read()}')
