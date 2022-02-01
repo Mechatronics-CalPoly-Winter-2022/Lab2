a606ba41d5111553713bcf265ea1a407f82c173c
kcjennin <kcjennin@calpoly.edu>
2022-02-01 12:33:41 -0800

    final changes

--- src/main.py
+++ src/main.py
@@ -22,57 +22,67 @@ def main():
 
     # initialize the servo
     servo = Servo(m_config, e_config)
-    servo.enable_motor()
     servo.zero()
 
-    # initialize the pid controller
-    # 17232 is 1/2 of the encoder
-    pid = PID(17000, float(input()))
+    try:
+        while True:
+            # initialize the pid controller
+            # 17232 is 1/2 of the encoder
+            in_val = float(input())
+            if in_val < 0:
+                break
+            pid = PID(17000, in_val)
+            servo.enable_motor()
+            print('running')
 
-    # initialize encoder and time data lists
-    encoder_data = []
-    time_data = []
+            # initialize encoder and time data lists
+            encoder_data = []
+            time_data = []
 
-    # run for 5 seconds
-    timeout = utime.ticks_add(utime.ticks_ms(), 5000)
-    while utime.ticks_diff(timeout, utime.ticks_ms()) > 0:
-        # get the error and adjust the duty cycle
-        servo.set_duty_cycle(pid.update(servo.get_error(pid.setpoint)))
+            # run for 5 seconds
+            timeout = utime.ticks_add(utime.ticks_ms(), 4000)
+            while utime.ticks_diff(timeout, utime.ticks_ms()) > 0:
+                # get the error and adjust the duty cycle
+                servo.set_duty_cycle(pid.update(servo.get_error(pid.setpoint)))
 
-        # get the encoder data and time data
-        encoder_data.append(servo.read())
+                # get the encoder data and time data
+                encoder_data.append(servo.read())
 
-        time_data.append(utime.ticks_add(utime.ticks_ms(), 0))
-
-
-        utime.sleep_ms(10)
-
-    print(f'ended at {encoder_data[-1]}')
-
-    # run it again
-    pid.set_setpoint(33000)
-    timeout = utime.ticks_add(utime.ticks_ms(), 5000)
-    while utime.ticks_diff(timeout, utime.ticks_ms()) > 0:
-        # get the error and adjust the duty cycle
-        servo.set_duty_cycle(pid.update(servo.get_error(pid.setpoint)))
-
-        # get the encoder data and time data
-        encoder_data.append(servo.read())
-        time_data.append(utime.ticks_add(utime.ticks_ms(), 0))
-
-        utime.sleep_ms(10)
-
-    servo.disable_motor()
-    
-    print(f'ended at {encoder_data[-1]}')
-
-    # reduce every element in time_data by the first element
-    time_data = [x - time_data[0] for x in time_data]
+                time_data.append(utime.ticks_add(utime.ticks_ms(), 0))
 
 
-    # print the encoder data and time data
-    pid.print_data(encoder_data, time_data)
+                utime.sleep_ms(10)
 
+            print(f'ended at {encoder_data[-1]}')
+
+            # run it again
+            pid.set_setpoint(33000)
+            timeout = utime.ticks_add(utime.ticks_ms(), 4000)
+            while utime.ticks_diff(timeout, utime.ticks_ms()) > 0:
+                # get the error and adjust the duty cycle
+                servo.set_duty_cycle(pid.update(servo.get_error(pid.setpoint)))
+
+                # get the encoder data and time data
+                encoder_data.append(servo.read())
+                time_data.append(utime.ticks_add(utime.ticks_ms(), 0))
+
+                utime.sleep_ms(10)
+
+            servo.disable_motor()
+
+            print(f'ended at {encoder_data[-1]}')
+
+            # reduce every element in time_data by the first element
+            time_data = [x - time_data[0] for x in time_data]
+
+
+            # print the encoder data and time data
+            pid.print_data(encoder_data, time_data)
+            servo.disable_motor()
+
+    except KeyboardInterrupt:
+        print('interrupted')
+        servo.disable_motor()
 
 if __name__ == '__main__':
     print('starting...')
